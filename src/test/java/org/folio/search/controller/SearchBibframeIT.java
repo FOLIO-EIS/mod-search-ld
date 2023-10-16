6d27ea0a001797cadb0dd88ecf48caffab3cfe88
Pavel Bobylev <131748689+PBobylev@users.noreply.github.com>
2023-10-16 14:35:48 +0600

    MODLD-89: linked data bibframe search support (#23)
    undefined

---
+++ src/test/java/org/folio/search/controller/SearchBibframeIT.java
@@ -0,0 +1,111 @@
+package org.folio.search.controller;
+
+import static org.folio.search.sample.SampleBibframe.getBibframe2SampleAsMap;
+import static org.folio.search.sample.SampleBibframe.getBibframeSampleAsMap;
+import static org.folio.search.sample.SampleBibframe.getBibframeSampleId;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+import org.folio.search.domain.dto.Bibframe;
+import org.folio.search.support.base.BaseIntegrationTest;
+import org.folio.spring.test.type.IntegrationTest;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+@IntegrationTest
+class SearchBibframeIT extends BaseIntegrationTest {
+
+  @BeforeAll
+  static void prepare() {
+    setUpTenant(Bibframe.class, getBibframeSampleAsMap(), getBibframe2SampleAsMap());
+  }
+
+  @AfterAll
+  static void cleanUp() {
+    removeTenant();
+  }
+
+  @DisplayName("search by bibframe (all 2 bibframe are found)")
+  @ParameterizedTest(name = "[{0}] {1}")
+  @CsvSource({
+    "1, cql.allRecords = 1",
+    "2, title all \"titleAbc\"",
+    "3, title any \"titleAbc\"",
+    "4, title any \"titleAbc def\"",
+    "5, title any \"titleAbc XXX\"",
+    "6, title = \"titleAbc\"",
+    "7, title <> \"titleXXX\"",
+    "8, title = \"title*\"",
+    "9, title = \"*\"",
+    "10, isbn <> \"1234\"",
+    "11, lccn <> \"2023\""
+  })
+  void searchByBibframe_parameterized_allResults(int index, String query) throws Throwable {
+    doSearchByBibframe(query)
+      .andExpect(jsonPath("$.totalRecords", is(2)));
+  }
+
+  @DisplayName("search by bibframe (single bibframe is found)")
+  @ParameterizedTest(name = "[{0}] {1}")
+  @CsvSource({
+    "1, title any \"def\"",
+    "2, title = \"titleAbc def\"",
+    "3, title == \"titleAbc def\"",
+    "4, title ==/string \"titleAbc def\"",
+    "5, isbn = \"*\"",
+    "6, isbn = \"1234567890123\"",
+    "7, isbn = \"1234*\"",
+    "8, isbn == \"1234567890123\"",
+    "9, isbn ==/string \"1234567890123\"",
+    "10, isbn any \"1234567890123\"",
+    "11, isbn any \"1234567890123 XXX\"",
+    "12, isbn all \"1234567890123\"",
+    "13, lccn = \"*\"",
+    "14, lccn = \"20232023\"",
+    "15, lccn = \"2023*\"",
+    "16, lccn == \"20232023\"",
+    "17, lccn ==/string \"20232023\"",
+    "18, lccn any \"20232023\"",
+    "19, lccn any \"20232023 XXX\"",
+    "20, lccn all \"20232023\""
+  })
+  void searchByBibframe_parameterized_singleResult(int index, String query) throws Throwable {
+    doSearchByBibframe(query)
+      .andExpect(jsonPath("$.totalRecords", is(1)))
+      .andExpect(jsonPath("$.content[0].id", is(getBibframeSampleId())));
+  }
+
+  @DisplayName("search by bibframe (nothing is found)")
+  @ParameterizedTest(name = "[{0}] {1}")
+  @CsvSource({
+    "1, title ==/string \"titleAbc\"",
+    "2, title ==/string \"def\"",
+    "3, title ==/string \"xyz\"",
+    "4, title == \"def titleAbc\"",
+    "5, title == \"titleAbcdef def\"",
+    "6, title all \"titleAbcdef\"",
+    "7, title any \"titleAbcdef\"",
+    "8, title = \"titleAbcdef\"",
+    "9, title <> \"titleAbc\"",
+    "10, isbn ==/string \"1234\"",
+    "11, isbn == \"1234\"",
+    "12, isbn any \"1234\"",
+    "13, isbn any \"12345678901231\"",
+    "14, isbn all \"1234\"",
+    "15, isbn = \"1234\"",
+    "16, lccn ==/string \"2023\"",
+    "17, lccn == \"2023\"",
+    "18, lccn any \"2023\"",
+    "19, lccn any \"202320231\"",
+    "20, lccn all \"2023\"",
+    "21, lccn = \"2023\""
+  })
+  void searchByBibframe_parameterized_zeroResults(int index, String query) throws Throwable {
+    doSearchByBibframe(query)
+      .andExpect(jsonPath("$.totalRecords", is(0)));
+  }
+
+}
