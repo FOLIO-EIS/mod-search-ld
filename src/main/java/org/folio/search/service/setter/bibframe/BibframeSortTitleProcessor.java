9134941290d7b9cedbf8826d45cb69a8e09c4ba0
pbobylev <pavel_bobylev@epam.com>
2024-01-29 20:55:29 +0600

    MODLD-229: Spike for searching Work with linked Instances

--- src/main/java/org/folio/search/service/setter/bibframe/BibframeSortTitleProcessor.java
+++ src/main/java/org/folio/search/service/setter/bibframe/BibframeSortTitleProcessor.java
@@ -1,10 +1,14 @@
 package org.folio.search.service.setter.bibframe;
 
-import static java.util.Objects.isNull;
+import static java.util.Optional.ofNullable;
 
+import java.util.Collection;
+import java.util.Objects;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import org.apache.commons.lang3.StringUtils;
 import org.folio.search.domain.dto.Bibframe;
+import org.folio.search.domain.dto.BibframeInstancesInner;
 import org.folio.search.domain.dto.BibframeTitlesInner;
 import org.folio.search.service.setter.FieldProcessor;
 import org.springframework.stereotype.Component;
@@ -14,10 +18,11 @@ public class BibframeSortTitleProcessor implements FieldProcessor<Bibframe, Stri
 
   @Override
   public String getFieldValue(Bibframe bibframe) {
-    if (isNull(bibframe.getTitles())) {
-      return null;
-    }
-    return bibframe.getTitles().stream()
+    var workTitles = ofNullable(bibframe.getTitles()).stream().flatMap(Collection::stream);
+    var instanceTitles = ofNullable(bibframe.getInstances()).stream().flatMap(Collection::stream)
+      .map(BibframeInstancesInner::getTitles).filter(Objects::nonNull).flatMap(Collection::stream);
+    return Stream.concat(workTitles, instanceTitles)
+      .filter(Objects::nonNull)
       .map(BibframeTitlesInner::getValue)
       .filter(StringUtils::isNotBlank)
       .collect(Collectors.joining());
